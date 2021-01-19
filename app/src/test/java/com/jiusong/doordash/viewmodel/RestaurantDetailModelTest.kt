e645df4f3e70434fde21d0707776b7efe2591dee
gaojiusong@gmail.com <gaojiusong@gmail.com>
2021-01-18 19:52:24 -0800

    Add unit test for RestaurantDetailModel.

---
+++ app/src/test/java/com/jiusong/doordash/viewmodel/RestaurantDetailModelTest.kt
@@ -0,0 +1,43 @@
+package com.jiusong.doordash.viewmodel
+
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import com.jiusong.doordash.FileReader
+import com.jiusong.doordash.data.Repository
+import com.jiusong.doordash.data.model.Store
+import com.jiusong.doordash.data.model.StoreFeedResponse
+import com.jiusong.doordash.data.network.DoorDashAPi
+import com.jiusong.doordash.data.network.DoorDashServiceFactory
+import com.jiusong.doordash.util.DoorDashConstants
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Created by jiusong.gao on 1/18/21.
+ */
+class RestaurantDetailModelTest {
+
+    private lateinit var viewModel: RestaurantDetailModel
+
+    @Before
+    fun setUp() {
+        val doorDashAPi = DoorDashServiceFactory.retrofit(DoorDashConstants.DOOR_DASH_API_BASE_URL).create(DoorDashAPi::class.java)
+        val repo = Repository(doorDashAPi)
+        viewModel = RestaurantDetailModel(repo)
+    }
+
+    @After
+    fun tearDown() {
+    }
+
+    @Test
+    fun getStoreRateDistanceText() {
+        val json = FileReader("stores_feed.json").content
+        val storeFeedResponse = Gson().fromJson(json, StoreFeedResponse::class.java)
+        val rateDistanceString = "4.7 • 6934+ ratings • 2.28 mi • $"
+        assertEquals(rateDistanceString, viewModel.getStoreRateDistanceText(storeFeedResponse.stores[0]))
+    }
+}
